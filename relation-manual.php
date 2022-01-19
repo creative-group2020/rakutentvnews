fb638d9000ae05c6125dfd4ec12bd0bee722a498
gonokami <ts-yoshikazu.gonokam@rakuten.com>
2022-01-19 15:20:44 +0900

    fix

---
+++ relation-manual.php
@@ -0,0 +1,38 @@
+<div class="rakutentv-news-relation-manual">
+  <div class="rakutentv-news-relation-manual-inr">
+
+    <div class="rakutentv-news-relation-manual-title">
+      <h2><span>関連記事</span></h2>
+    </div>
+
+    <div class="rakutentv-news-relation-manual-list">
+      <ol>
+        <li>
+          <a href="https://news.tv.rakuten.co.jp/2021/07/tbs-1dcu.html">
+            <span>阿部寛、TBS日曜劇場で再び主演!! 来年1月スタートの『DCU』で“手錠を持ったダイバー”に！</span>
+          </a>
+        </li>
+        <li>
+          <a href="https://news.tv.rakuten.co.jp/2021/08/210831dcu.html">
+            <span>横浜流星がダイバー役として出演決定！阿部寛主演のTBS日曜劇場の新ドラマ『DCU』</span>
+          </a>
+        </li>
+        <li>
+          <a href="https://news.tv.rakuten.co.jp/2021/09/210928dcu.html">
+            <span>山崎育三郎、趣里らが出演！阿部寛主演の新ドラマ『DCU』の追加キャスト陣が発表</span>
+          </a>
+        </li>
+        <li>
+          <a href="https://news.tv.rakuten.co.jp/2021/10/211024dcu.html">
+            <span>中村アン、2022年1月スタートの阿部寛主演ドラマ『DCU』にダイバー役で出演！</span>
+          </a>
+        </li>
+        <li>
+          <a href="https://news.tv.rakuten.co.jp/2021/10/211027dcu.html">
+            <span>岡崎体育が6人目のダイバー役に！阿部寛主演ドラマ『DCU』への出演が決定</span>
+          </a>
+        </li>
+      </ol>
+    </div>
+  </div>
+</div>
