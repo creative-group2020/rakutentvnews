9c4333a3351c43c02281d530cd1f0342d2c822f1
gonokami <ts-yoshikazu.gonokam@rakuten.com>
2022-01-21 19:03:27 +0900

    fix

---
+++ article/relation_manual.php
@@ -0,0 +1,20 @@
+<div class="rakutentv-news-relation-manual">
+  <div class="rakutentv-news-relation-manual-inr">
+
+    <div class="rakutentv-news-relation-manual-title">
+      <h2 id="chapter-1"><span>関連記事</span></h2>
+    </div>
+
+    <div class="rakutentv-news-relation-manual-data" style="display:none;">
+      https://news.tv.rakuten.co.jp/2021/07/tbs-1dcu.html
+      阿部寛、TBS日曜劇場で再び主演!! 来年1月スタートの『DCU』で“手錠を持ったダイバー”に！
+      https://news.tv.rakuten.co.jp/2021/08/210831dcu.html
+      横浜流星がダイバー役として出演決定！阿部寛主演のTBS日曜劇場の新ドラマ『DCU』
+      https://news.tv.rakuten.co.jp/2021/09/210928dcu.html
+      山崎育三郎、趣里らが出演！阿部寛主演の新ドラマ『DCU』の追加キャスト陣が発表
+      https://news.tv.rakuten.co.jp/2021/10/211024dcu.html
+      中村アン、2022年1月スタートの阿部寛主演ドラマ『DCU』にダイバー役で出演！
+    </div>
+    <div class="rakutentv-news-relation-manual"></div>
+  </div>
+</div>
